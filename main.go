f3c0db34f459e3df6105d8c9e5921eda056d5212
Mateusz Kyc <mateusz.kyc@gmail.com>
2024-02-29 16:03:10 +0100

    make super simple version which can run external executables

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 )
 
 func main() {
@@ -21,4 +24,36 @@ func main() {
 	pluginArgs := os.Args[2:]
 
 	fmt.Println("Plugin arguments: ", pluginArgs)
+
+	cmd := exec.Command(pluginExecutable, pluginArgs...)
+	fmt.Println("Executing command: ", cmd)
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error creating StdoutPipe for Cmd:", err)
+		return
+	}
+
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("Error creating StderrPipe for Cmd:", err)
+		return
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting Cmd:", err)
+		return
+	}
+
+	multiReader := io.MultiReader(stdoutPipe, stderrPipe)
+	scanner := bufio.NewScanner(multiReader)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Cmd finished with error:", err)
+		return
+	}
 }
