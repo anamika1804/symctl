fb64b3663e02389af85eae1e4b73894bcbd46ce3
Mateusz Kyc <mateusz.kyc@gmail.com>
2024-03-01 11:22:16 +0100

    Installer step 1

--- internal/installer/installer.go
+++ internal/installer/installer.go
@@ -1,7 +1,17 @@
 package installer
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Install(address string, args []string) {
 	fmt.Printf("Installing plugin from %s with args %v\n", address, args)
+
+	executablePath, err := os.Executable()
+	if err != nil {
+		fmt.Printf("Error getting executable path: %s\n", err)
+		return
+	}
+	fmt.Printf("Executable path: %s\n", executablePath)
 }
