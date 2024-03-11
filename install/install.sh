2b72b98818091b67f73214b21b24800fd3f9a426
Mateusz Kyc <mateusz.kyc@gmail.com>
2024-03-11 13:59:47 +0100

    add mock install script

---
+++ install/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Installing symctl..."
+# some mock installation output
+sleep 1
+echo "1/3"
+sleep 1
+echo "2/3"
+sleep 1
+echo "3/3"
+sleep 1
+echo "Installed symctl!"
